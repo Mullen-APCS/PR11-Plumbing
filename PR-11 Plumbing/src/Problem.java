142c67a1ceca426b0cb2183733b0e5d6c873c28c
Mike McGuire <mcguire@mullenhigh.com>
2014-08-28 07:20:00 -0600

    Initial Commit

---
+++ PR-11 Plumbing/src/Problem.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+   
+class Problem
+{
+   public static void main (String[] args) throws IOException
+   {
+      Scanner file = new Scanner( new File( "pr11.dat" ) );
+      int numberOfLinesOfData = file.nextInt();      
+      file.nextLine();
+      for( int i = 0; i < numberOfLinesOfData; i++ )
+      {
+           // Your code goes here
+      }
+   }
+}
